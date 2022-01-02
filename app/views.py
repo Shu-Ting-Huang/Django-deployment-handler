d6f45750ab7cfe8bc9479304f9086157d85327fc
Shu-Ting Huang <sthuang890702@gmail.com>
2022-01-01 22:10:51 -0500

    Show the server's external IP on the hello world page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
+import os
 from django.shortcuts import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!!")
+    return HttpResponse("Hello, world!! \n<br>The external IP is " + os.environ['EXTERNAL_IP'])
