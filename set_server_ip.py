91eb91d5e45037eb41c1295b4091b98cdcba7a0e
Shu-Ting Huang <sthuang890702@gmail.com>
2022-01-27 02:52:09 -0500

    Update the server IP in server_info.json

--- set_server_ip.py
+++ set_server_ip.py
@@ -1,5 +1,15 @@
 import os
+import json
+
+# Get server IP from user
 server_ip = input("Enter the server IP: ")
 
 # change to the directory in which THIS SCRIPT is located
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# update the server IP in server_info.json
+with open("server_info.json", "r") as f:
+    server_info = json.load(f)
+server_info["server_ip"] = server_ip
+with open("server_info.json", "w") as f:
+    f.write(json.dumps(server_info, indent = 4))
