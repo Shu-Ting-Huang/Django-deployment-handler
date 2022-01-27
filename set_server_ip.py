3a0b431559d9c427b68e4fb3f3d2d69cdc3be718
Shu-Ting Huang <sthuang890702@gmail.com>
2022-01-27 14:23:39 -0500

    Move server_info.json to the parent directory

--- set_server_ip.py
+++ set_server_ip.py
@@ -8,18 +8,21 @@ server_ip = input("Enter the server IP: ")
 # change to the directory in which THIS SCRIPT is located
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# update the server IP in server_info.json
-with open("server_info.json", "r") as f:
-    server_info = json.load(f)
-server_info["server_ip"] = server_ip
-with open("server_info.json", "w") as f:
+# update the server IP in ../server_info.json
+try:
+    with open("../server_info.json", "r") as f:
+        server_info = json.load(f)
+        server_info["server_ip"] = server_ip
+except FileNotFoundError:
+    server_info = {"server_ip": server_ip, "deployment_port": 1234}
+with open("../server_info.json", "w") as f:
     f.write(json.dumps(server_info, indent = 4))
 del server_info
 
 # set up GitHub actions script
 os.makedirs("../.github/workflows", exist_ok = True) # create this directory if not exist
 with open("../.github/workflows/deploy.yml", "w") as f:
-    with open("server_info.json", "r") as g:
+    with open("../server_info.json", "r") as g:
         server_info = json.load(g)
     with open("template.yml", "r") as g:
         src = Template(g.read())
